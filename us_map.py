ced4feb8f579f5312d99e5dc53c7e31f78b3639d
kayan19 <kayan@ctemc.org>
2018-05-02 14:17:21 -0400

    color map
    map in color

--- us_map.py
+++ us_map.py
@@ -1,18 +1,10 @@
+import numpy as np
 from mpl_toolkits.basemap import Basemap
 from matplotlib.colors import rgb2hex
 from matplotlib.patches import Polygon
 import matplotlib.pyplot as plt
 import os.path
 import csv
-
-fig, ax = plt.subplots(1,1, figsize=(16,8))
-usMap = Basemap(ax = ax, 
-            resolution='c', 
-            llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49,
-            projection='lcc', lat_1=33, lat_2=45, lon_0=-95)
-usMap.drawcoastlines()
-usMap.drawcountries()
-usMap.drawstates()
 
 directory = os.path.dirname(os.path.abspath(__file__))
 filename = os.path.join(directory, 'banklist.csv') 
@@ -29,5 +21,42 @@ for row in datareader:
     else:
         failures[key]=1
 print failures
+
+fig, ax = plt.subplots(1,1, figsize=(16,8))
+usMap = Basemap(ax = ax, 
+            resolution='c', 
+            llcrnrlon=-119, llcrnrlat=22, urcrnrlon=-64, urcrnrlat=49,
+            projection='lcc', lat_1=33, lat_2=45, lon_0=-95)
+
+usMap.readshapefile('st99_d00', name='states', drawbounds=True)
+
+colors={}
+statenames=[]
+vmax = 105.
+
+for shapedict in usMap.states_info:
+    statename = shapedict['NAME']
+    if statename in failures:
+        pop = failures[statename]
+        if pop>40:
+            colors[statename] = '#4390bc'
+        elif pop>=12:
+            colors[statename] = '#68a7ca'
+        elif pop>=7:
+            colors[statename] = '#8dbdd8'
+        elif pop>=3:
+            colors[statename] = '#b2d3e6'
+        else:
+            colors[statename] = '#d8e9f3'
+    else:
+        colors[statename] = '#d8e9f3'
+       
+    statenames.append(statename)
+
+ax = plt.gca()
+for shape, seg in enumerate(usMap.states):
+    color = colors[statenames[shape]]
+    poly = Polygon(seg, facecolor=color, edgecolor=color)
+    ax.add_patch(poly)
 
 fig.show()
