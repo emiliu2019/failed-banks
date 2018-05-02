56a98be4da3e23207f05b64bba0a6e8f2fed00f2
emiliu2019 <emiliu@ctemc.org>
2018-05-02 14:20:23 -0400

    Delete stuff

--- histogram_failed_banks_years.py
+++ histogram_failed_banks_years.py
@@ -37,7 +37,7 @@ headers = datareader.next() # read first row and store separately
 years=[]
 dates=[]
 number_banks=[]
-number=0
+
 for row in datareader:
     year= ((row[5]))
     if ('20'+year[-2:] not in years):
@@ -52,35 +52,15 @@ for year in years:
     number_banks.append(number)
     number =0
 print number_banks
-# 
-# # for line in data[1:]: # Omit 0, 1, 2 because _______
-# #     age, income = line.split(',')
-# # 
-# #     # ___________ the age data
-# #     ages.append(int(age))
-# # 
-# #     # __________ the income data
-# #     if '-' in income:
-# #         # Do not use the first 3 characters: space, -, and $
-# #         # and do not use the last character: \n
-# #         years.append(-1*int(income[-2:]))
-# #     else:
-# #         # Do not use the first 2 characters: space and $
-# #         # and do not use the last character: \n
-# #         incomes.append(int(income[2:-1]))         
-# 
-# ###
-# # Create the histograms
-# ###
 
+#Create scatterplot 
 fig, ax  = plt.subplots(1, 1)
-ax.plot(years, number_banks, '-o')
+ax.plot(years, number_banks, '-o') #'-o' connects points, ro leaves them discrete
 ax.set_title('Number of Failed Banks vs. Year\n(2000-2017 U.S. Sample)')
 ax.set_xlabel('Year')
 ax.set_ylabel('Number of Failed Banks')
 
 fig.show()
-
 
 #Histogram 1
 fig_year, ax  = plt.subplots(1, 1)
