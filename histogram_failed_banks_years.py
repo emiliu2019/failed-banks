efb3576f8769df19d5ff6af01eaf51179e246c3c
emiliu2019 <emiliu@ctemc.org>
2018-05-04 19:58:51 -0400

    Create bar graph

--- histogram_failed_banks_years.py
+++ histogram_failed_banks_years.py
@@ -55,19 +55,23 @@ for year in years:
     number_banks.append(number)
     number =0
 
-# #Create scatterplot 
-# fig_year, ax  = plt.subplots(1, 1)
-# ax.plot(years, number_banks, '-o') #'-o' connects points, ro leaves them discrete
-# ax.set_title('Number of Failed Banks vs. Year\n(2000-2017 U.S. Sample)')
-# ax.set_xlabel('Year')
-# plt.xticks(np.linspace(2000, 2017, 18, endpoint=True))
-# ax.set_ylabel('Number of Failed Banks')
-# fig_year.show()
+#Create scatterplot 
+fig_year, ax  = plt.subplots(1, 1)
+ax.plot(years, number_banks, '-o') #'-o' connects points, ro leaves them discrete
+ax.set_title('Number of Failed Banks vs. Year\n(2000-2017 U.S. Sample)')
+ax.set_xlabel('Year')
+plt.xticks(np.linspace(2000, 2017, 18, endpoint=True))
+ax.set_ylabel('Number of Failed Banks')
+fig_year.show()
 
+years = list(reversed(years))
+print years
 #Bar graph
 fig, ax=plt.subplots(1,1)
 width = 0.35
 bar_graph=ax.bar([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16], number_banks, width, align='center', color='blue')
+ax.set_xticks(np.arange(17)+1)
+ax.set_xticklabels(years)
 ax.set_ylabel("Number of Failed Banks")
 ax.set_xlabel("Year")
 ax.set_title("Failed Bank Distribution in US By Year")
